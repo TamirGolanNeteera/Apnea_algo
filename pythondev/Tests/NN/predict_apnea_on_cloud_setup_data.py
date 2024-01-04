ddf463f0bd7fef6c1b72a1f02477c1e84134e279
Tamir Golan <tamir.golan@neteera.com>
2024-01-04 13:27:47 +0200

    bugfix

--- pythondev/Tests/NN/predict_apnea_on_cloud_setup_data.py
+++ pythondev/Tests/NN/predict_apnea_on_cloud_setup_data.py
@@ -347,7 +347,7 @@ if __name__ == '__main__':
         # yair_ahi_dict[session] = yair_ahi
         dana_ahi_dict[session] = pahi_from_y
         session_ahi_pdf_dict = {108139: 1.2, 108145: 10.8, 108186: 56.6, 108168: 37.6, 108146: 0.8, 108147: 6.1, 108148: 29.2, 108153: 5.4, 108154: 3.8, 108152: 16.2, 108170: 2.7, 108171: 13.7, 108175: 0.4, 108207: 5.5, 108191: 9.6, 108192: 4.1, 108201: 13.6, 108223: 8.9, 108298: 23.1, 108222: 5.2, 108226: 1.9, 108299: 11.8, 108303: 1.2, 108331: 3.7, 108348: 15.8, 108335: 9.8, 108349: 48.2}
-        num_apneas_from_pred = 5 * np.sum(preds)
+        num_apneas_from_pred = np.sum(preds)
         pahi_from_pred = 4*num_apneas_from_pred/len(preds)
 
         len_valid = len(valid[valid == 1])
